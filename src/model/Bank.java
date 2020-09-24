0dbc33cb19c450e2b5e02d867717ba3aebe0fd98
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2020-09-24 09:27:46 -0500

    Generic sort method added

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -25,5 +25,24 @@ public class Bank {
 	}
 	
 	
+	public <E extends Comparable<E>> void selectionSort(E[] list) {
+		
+		 for(int i=0; i<list.length -1; i++) {
+			 
+	            int iSmallest = i;
+
+	            for(int j=i+1; j<list.length; j++)
+	            {
+	                if(list[iSmallest].compareTo((list[j])) > 0  )
+	                {
+	                    iSmallest = j;
+	                }
+	            }
+	            E iSwap = list[iSmallest];
+	            list[iSmallest] = list[i];
+	            list[i] = iSwap;
+	        }
+	}
+	
 
 }
