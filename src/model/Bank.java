acf00a747cf192ac7898995808769d7d376815e9
alejandroarce03 <54712283+alejandroarce03@users.noreply.github.com>
2020-09-27 21:42:20 -0500

    MergeSort

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -2,28 +2,40 @@ package model;
 
 import HashTable.HashTable;
 
+/**This is the Main Class of the Bank Simply Organizer Project 
+ * which is mean to be a simple organizer for attending Clients in a bank.
+ * 
+ * @authors Alejandro Arce, Alexander Samaca, Duvan Cuero
+ * 
+ * @date Unreleased
+ * 
+ */
 public class Bank {
 	
 	HashTable<Integer,Client> bank;
-	Administrator admin;
 	
 	/** The constructor of the Class bank which starts with a designate administrator
 	 *  which takes control of the .
 	 * @param admin: The administrator of the bank.
 	 */
-	public Bank(Administrator admin) {
-		bank = new HashTable<Integer,Client>(0);
-		this.admin=admin;
+	public Bank() {
+		bank = new HashTable<Integer,Client>(12);
 	}
 	
 	/** Searches a client in the Bank Based on his cc.
-	 * @param cc
-	 * @return
+	 * @param cc : the key of search in the HashTable
+	 * @return Client if it was found or an exception if the client doesn't exist
 	 */
-	public Client searchClientCc(Integer cc) {	
+	public Client searchClient(Integer cc) {	
 		return bank.get(cc);
 	}
 	
+	/** Removes a client from the Bank canceling his account.
+	 * @param cc : The key of search and erase of the client.
+	 * @return if the Client was found returns the Clients object, else returns and exception.
+	 */
+	public Client cancelAcount(Integer cc) {
+		return bank.remove(cc);
+	}
 	
-
 }
---
+++ src/sortmethods/SortingMetdos.java
@@ -0,0 +1,101 @@
+package sortmethods;
+
+import BTS.*;
+
+public  class SortingMetdos  {
+	
+	/** Selection  sort generic implementation
+	 * @param <E> : Generic use of the array
+	 * @param list: the array that is going to be sort
+	 */
+	public static <E extends Comparable<E>> void selectionSort(E[] list) {
+		
+		 for(int i=0; i<list.length -1; i++) {
+			 
+	            int iSmallest = i;
+
+	            for(int j=i+1; j<list.length; j++)
+	            {
+	                if(list[iSmallest].compareTo((list[j])) > 0  )
+	                {
+	                    iSmallest = j;
+	                }
+	            }
+	            E iSwap = list[iSmallest];
+	            list[iSmallest] = list[i];
+	            list[i] = iSwap;
+	     
+	        }
+	}
+	
+	public <E extends Comparable<E>> void  mergeSort(E[] list,int start,int end) {
+		
+		if  (list.length <= 1) {
+			
+			throw new IllegalArgumentException("There is only 1 account or is null");
+				
+		}else {
+			int middle = list.length/2;
+			mergeSort(list,start,middle);
+			mergeSort(list,middle+1,end);
+			
+			merge(list,start,middle,end);
+			
+		}
+		
+		
+	}
+	
+
+	public <E extends Comparable<E>> void merge(E[] list,int start, int middle, int end) {
+		E[] left  = (E[]) new Comparable[middle - start+1];
+		E[] right = (E[]) new Comparable[end - middle];
+
+		for (int i = 0; i < left.length; ++i)
+			left[i] = list[start+i];
+
+		for (int i = 0; i < right.length; ++i)
+			right[i] = list[middle + i+1];
+
+		int leftI= 0;
+		int rightI = 0;
+		int currentI = 0;
+
+		while (leftI < left.length && rightI < right.length)
+		{
+			if (( left[leftI]).compareTo( right[rightI]) <= 0)
+			{
+				list[currentI] = left[leftI];
+				leftI++;
+			}
+			else
+			{
+				list[currentI] = right[rightI];
+				rightI++;
+			}
+			currentI++;
+		}
+	
+		while (leftI < left.length) {
+			currentI++;
+			leftI++;
+			list[currentI] = left[leftI];
+		}
+
+		while (rightI < right.length) {
+			currentI++;
+			rightI++;
+			list[currentI] = right[rightI];
+		}
+	}
+  
+	
+	public static <E extends Comparable<E>> void Tree(E[] list) {
+		BTS<E> tree = new BTS<E>();
+		for(int i=0;i<list.length;i++) {
+			tree.addNode(list[i]);
+		}
+	}
+	
+
+}
