c0af0b94d0a0c0c44a253650c306b856d24fb259
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2020-09-29 18:56:12 -0500

    Making bank look nice and thick

---
+++ Bank Simple Organizer/src/sortmethods/SortingMetdos.java
@@ -0,0 +1,52 @@
+package sortmethods;
+
+import BTS.*;
+
+public  class SortingMetdos {
+	
+	/** Selection  sort generic implementation
+	 * @param <E> : Generic use of the array
+	 * @param list: the array that is going to be sort
+	 */
+	public static <E extends Comparable<E>> void selectionSort(E[] list) {
+		
+		 for(int i=0; i<list.length -1; i++) {
+			 
+	            int iSmallest = i;
+
+	            for(int j=i+1; j<list.length; j++)
+	            {
+	                if(list[iSmallest].compareTo((list[j])) > 0  )
+	                {
+	                    iSmallest = j;
+	                }
+	            }
+	            E iSwap = list[iSmallest];
+	            list[iSmallest] = list[i];
+	            list[i] = iSwap;
+	     
+	        }
+	}
+	
+	public static <E extends Comparable<E>> void mergeSort(E[] list) {
+		//Alex acabar!!
+		
+		if  (list.length <= 1) {
+			
+			throw new IllegalArgumentException("There is only 1 account or is null");
+			
+			
+		}
+		
+	}
+	
+	public static <E extends Comparable<E>> void Tree(E[] list) {
+		BTS<E> tree = new BTS<E>();
+		for(int i=0;i<list.length;i++) {
+			tree.addNode(list[i]);
+		}
+	}
+	
+	
+
+}
